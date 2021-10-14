119ab1e872a01fcb3edb7176520373128fc961d0
Abhinov <75292530+ippudkippude@users.noreply.github.com>
2021-10-14 22:22:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,89 @@
-from enum import Enum
-from typing import Optional
-import os
-from fastapi import FastAPI, Request, Form, Query
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from src.dac_filters import StoreClassifiers as scs
-from fastapi.responses import HTMLResponse
+#imports
+from typing import Dict, List
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import json
+
+#defining class objects for ML predictors and classifiers
+
+# object model that helps absorb predictors input parameters
+class Predict_struct(BaseModel):
+    model_number : int
+    year : int
+    mileage : int
+    tax : int
+    mpg : int
+    enginesize : int
+    automatic : int
+    manual : int
+    semiauto : int
+    diesel : int
+    hybrid : int
+    other : int
+    petrol : int
+
+# object model that helps common input
+class Common_struct(BaseModel):
+    predict : str
+    classify : str
+    predict_input_set : Predict_struct
 
 
-app = FastAPI()
-templates = Jinja2Templates(directory=os.path.abspath(os.path.expanduser('templates')))
-scsobj = scs()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# object model that helps absorb classfiers input parameters
+class Classify_struct(BaseModel):
+    name : str
 
-#Calling DAClass for Mysql>Df conversion: Returns 2 DFs
-orders,customers = scs.generate_df_from_mysql()
+#data access classes
+# SETUP CLASSES CALL
 
-class serviceChoices(str, Enum):
-    svc = "svc"
-    trc = "trc"
-    lbc = "lbc"
-    totr =  "totr"
+from core.models.price_predictor import PredictPrice as ppr
+# Load model once
+ppr.load_xgb_model()
 
-@app.get("/")
-def root_go():
-    return {"message": "Go step 2"}
+# sample input
+# format: 	model	year   mileage	tax	mpg	engineSize	Automatic	Manual	Semi-Auto	Diesel	Hybrid	Other	Petrol
+sample_input = [[5,2019,516,150,33.2,2.0,0,1,0,0,0,0,1]]
 
+mlapp = FastAPI()
 
-@app.get("/form",response_class=HTMLResponse)
-def form_post(request: Request):
-    result = "Type a service name"
-    return templates.TemplateResponse('result.html', context={'request': request, 'result': result})
+# Rolling callers
+# base call
+@mlapp.get("/v1/")
+def base():
+    return {"message":" This is the base for ML apps"}
 
+@mlapp.post("/v1/")
+async def postbase(request: Request):
+    return {"message":" This is the base for ML apps"}
 
+# post call
+@mlapp.post("/v1/algos")
+async def ml_decision(incoming_json : Common_struct):
+    mn = json.loads(incoming_json.json())
+    #print(mn['predict_input_set']['year'])
+    print(type(mn['predict_input_set']['year']))
 
-@app.post("/form",response_class=HTMLResponse)
-def form_post(request: Request,
-              classi_dd: serviceChoices = Form(serviceChoices.svc),
-              ):
+    to_predict_list = [ mn['predict_input_set']['model_number'],
+    mn['predict_input_set']['year'],
+    mn['predict_input_set']['mileage'],
+    mn['predict_input_set']['tax'],
+    mn['predict_input_set']['mpg'],
+    mn['predict_input_set']['enginesize'],
+    mn['predict_input_set']['automatic'],
+    mn['predict_input_set']['manual'],
+    mn['predict_input_set']['semiauto'],
+    mn['predict_input_set']['diesel'],
+    mn['predict_input_set']['hybrid'],
+    mn['predict_input_set']['other'],
+    mn['predict_input_set']['petrol']
+    ]
 
-    if classi_dd.strip() ==  "lbc":
-        result = scsobj.get_loyal_member(customers)
-    elif classi_dd == "svc":
-        result = scsobj.get_senior_customer(customers)
-    elif classi_dd == "trc":
-        result = scsobj.get_top_revenue_customer(orders,customers)
-    elif classi_dd == "totr":
-        result = scsobj.get_total_revenue(orders)
-    else:
-        result = "Wrong Input"
+    result_price = ppr.get_sp([to_predict_list])
 
-    html_content = """
-        <html>
-            <head>
-                <title>Result is : </title>
-            </head>
-            <body>
-                <h3>"""+result+"""</h3>
-            </body>
-        </html>
-        """
-    return HTMLResponse(content=html_content, status_code=200)
+    print("Sale price is : ",result_price) #)incoming_json.json())
+    return_dict = { "message" : " Below is the approx current value for your toyota",
+            "Sale_Price": str(result_price) }
+
+    return  json.dumps(return_dict) #incoming_json.json()
+
 
